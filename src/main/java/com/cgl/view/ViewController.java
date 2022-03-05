4491638e1e29a605e3d81957c58e2f25728e82cb
qbou99 <qbou@outlook.fr>
2022-03-05 09:49:58 +0100

    Thymeleaf

---
+++ src/main/java/com/cgl/view/ViewController.java
@@ -0,0 +1,16 @@
+package com.cgl.view;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/")
+public class ViewController {
+
+    @GetMapping
+    public String index() {
+        return "index";
+    }
+}
