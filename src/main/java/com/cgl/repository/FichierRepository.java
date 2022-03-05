5632b2e8896a8cc6a3642579d287049dd7614004
johannconicella <johannconicella@gmail.com>
2022-03-05 12:19:32 +0100

    get by fileType

--- src/main/java/com/cgl/repository/FichierRepository.java
+++ src/main/java/com/cgl/repository/FichierRepository.java
@@ -1,6 +1,7 @@
 package com.cgl.repository;
 
 import com.cgl.model.Fichier;
+import com.cgl.model.TypeFichier;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.Optional;
@@ -8,5 +9,5 @@ import java.util.Optional;
 public interface FichierRepository extends JpaRepository<Fichier, Long> {
     Optional<Fichier> findById(Long id);
 
-    Fichier findByTypeFichier(String typeFichier);
+    Fichier findByTypeFichier(TypeFichier typeFichier);
 }
