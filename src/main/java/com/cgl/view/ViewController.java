9a5185dab4dd6f9a9865432e46a07257f1afb137
qbou99 <qbou@outlook.fr>
2022-03-14 23:35:23 +0100

    WIP

--- src/main/java/com/cgl/view/ViewController.java
+++ src/main/java/com/cgl/view/ViewController.java
@@ -1,5 +1,7 @@
 package com.cgl.view;
 
+import com.cgl.repository.FichierRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -9,18 +11,23 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @RequestMapping("/")
 public class ViewController {
 
+    @Autowired
+    FichierRepository fichierRepository;
+
     @GetMapping
     public String index() {
         return "index";
     }
 
     @GetMapping(value="/docs_list")
-    public String docs_list() {
+    public String docs_list(Model model) {
+        model.addAttribute("fichiers", fichierRepository.findAll());
         return "docs_list";
     }
 
     @GetMapping(value="/new_doc")
-    public String new_doc() {
+    public String new_doc(Model model) {
+        model.addAttribute("fichier", new Fichier());
         return "new_doc";
     }
 
