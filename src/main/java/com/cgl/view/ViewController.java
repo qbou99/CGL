f5deef0989966f548572cc8e98149bbb548fed3a
qbou99 <qbou@outlook.fr>
2022-03-14 23:38:30 +0100

    Oups

--- src/main/java/com/cgl/view/ViewController.java
+++ src/main/java/com/cgl/view/ViewController.java
@@ -1,5 +1,6 @@
 package com.cgl.view;
 
+import com.cgl.model.Fichier;
 import com.cgl.repository.FichierRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
