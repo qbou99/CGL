59d2fcd24d80460f2b601bfd0021ba5e3a84fba7
qbou99 <qbou@outlook.fr>
2022-03-18 21:14:09 +0100

    Tests

---
+++ src/test/java/com/cgl/controller/FichierControllerTest.java
@@ -0,0 +1,116 @@
+package com.cgl.controller;
+
+import com.cgl.dto.FichierDto;
+import com.cgl.exception.ResourceNotFoundException;
+import com.cgl.model.Fichier;
+import com.cgl.model.TypeFichier;
+import com.cgl.repository.FichierRepository;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.ArgumentCaptor;
+import org.mockito.Captor;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.web.servlet.view.RedirectView;
+
+import java.io.File;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Optional;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+@ExtendWith(MockitoExtension.class)
+class FichierControllerTest {
+
+    @InjectMocks
+    FichierController fichierController;
+
+    @Mock
+    FichierRepository fichierRepository;
+
+    @Captor
+    ArgumentCaptor<Fichier> fichierArgumentCaptor;
+
+    @Test
+    void givenFichiers_whenGetAll_thenFichiers() {
+        Fichier f1 = new Fichier("/user/fichier1", "Fichier 1", TypeFichier.Image);
+        Fichier f2 = new Fichier("/user/fichier2", "Fichier 2", TypeFichier.Audio);
+        List<Fichier> fichiers = Arrays.asList(f1, f2);
+
+        when(fichierRepository.findAll()).thenReturn(fichiers);
+
+        List<Fichier> result = fichierController.getAllFichiers();
+
+        assertThat(result).hasSize(fichiers.size());
+        assertThat(result.get(0).getChemin()).isEqualTo(f1.getChemin());
+        assertThat(result.get(1).getChemin()).isEqualTo(f2.getChemin());
+    }
+
+    @Test
+    void givenFichier1_whenGet1_thenFichier1() {
+        Fichier f1 = new Fichier("/user/fichier1", "Fichier 1", TypeFichier.Image);
+
+        when(fichierRepository.findById(1L)).thenReturn(Optional.of(f1));
+
+        Fichier result = fichierController.getFichierById(1L);
+
+        assertThat(result.getChemin()).isEqualTo(f1.getChemin());
+    }
+
+    @Test
+    void givenUnknownFichierId_whenGetById_thenException() {
+        when(fichierRepository.findById(1337L)).thenReturn(Optional.empty());
+
+        ResourceNotFoundException e = assertThrows(ResourceNotFoundException.class, () -> fichierController.getFichierById(1337L));
+
+        assertThat(e.getResourceName()).isEqualTo("Fichier");
+        assertThat(e.getFieldName()).isEqualTo("id");
+        assertThat(e.getFieldValue()).isEqualTo(1337L);
+    }
+
+    @Test
+    void givenFichier_whenSave_thenFichierSaved() {
+        FichierDto f = new FichierDto("/user/fichier", "Fichier 1", TypeFichier.Audio);
+
+        when(fichierRepository.save(any(Fichier.class))).thenReturn(f.dtoToEntity());
+
+        RedirectView result = fichierController.archiveFichier(new File("/user/fichier"), "Fichier 1", "Audio");
+
+        verify(fichierRepository).save(fichierArgumentCaptor.capture());
+
+        assertThat(fichierArgumentCaptor.getValue().getNom()).isEqualTo(f.getNom());
+    }
+
+    @Test
+    void givenFichier_whenDelete_thenFichierDeleted() {
+        Fichier f1 = new Fichier("/user/fichier1", "Fichier 1", TypeFichier.Image);
+
+        when(fichierRepository.findById(1L)).thenReturn(Optional.of(f1));
+
+        RedirectView result = fichierController.deleteFichier(1L);
+
+        verify(fichierRepository).delete(fichierArgumentCaptor.capture());
+
+        assertThat(fichierArgumentCaptor.getValue().getNom()).isEqualTo(f1.getNom());
+    }
+
+    @Test
+    void givenNom_whenSearchByName_thenFichierLikeName() {
+        Fichier f1 = new Fichier("/user/fichier1", "Fichier 1", TypeFichier.Image);
+
+        when(fichierRepository.findByNomContaining("Fichier", PageRequest.of(0, 10))).thenReturn(List.of(f1));
+
+        RedirectView result = fichierController.getFichierContainingNom("Fichier");
+
+        verify(fichierRepository).findByNomContaining(fichierArgumentCaptor.capture());
+
+        assertThat(fichierArgumentCaptor.getValue().getNom()).isEqualTo(f1.getNom());
+    }
+}
